86562e3555b1c0e8bcae96b8a2133ce0cb352915
Fukusan <ikishikawa64@gmail.com>
2017-12-25 17:20:58 +0900

    色々手直し

--- BarcodeReader/js/main.js
+++ BarcodeReader/js/main.js
@@ -7,7 +7,9 @@ window.onload = () =>{
 			this.h = h;
 			this.target = target;
 			Quagga.onDetected(data => this._detectedHandler(data));
+			Quagga.onProcessed(data => this._processedHandler(data));
 		}
+
 		start(){
 			Quagga.init({
 				numOfWorkers: 4,
@@ -32,12 +34,13 @@ window.onload = () =>{
 				Quagga.start();
 			});
 		}
+
 		stop(){
 			Quagga.stop();
 		}
+
 		async _detectedHandler(data){
 			let code = data.codeResult.code;
-			console.log(`code : ${code}`);
 			let codeArray = code.toString().split('').map((d)=>parseInt(d, 10));
 			if(this._checkDigit(codeArray)){
 				this.stop();
@@ -45,10 +48,42 @@ window.onload = () =>{
 				if(restart)this.start();
 			}
 		}
+
+		_processedHandler(result){
+			let drawingCtx = Quagga.canvas.ctx.overlay,
+				drawingCanvas = Quagga.canvas.dom.overlay;
+
+			if (result) {
+				if (result.boxes) {
+					drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+					result.boxes.filter(box => {
+						return box !== result.box;
+					}).forEach(box => {
+						Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 2});
+					});
+				}
+
+				if (result.box) {
+					Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "blue", lineWidth: 2});
+				}
+
+				if (result.codeResult && result.codeResult.code) {
+					Quagga.ImageDebug.drawPath(result.line, {x: 'x', y: 'y'}, drawingCtx, {color: 'red', lineWidth: 3});
+				}
+			}
+		}
+
 		_checkDigit(codeArray){
+			if(codeArray.length != 13)return false;
+			if(
+				codeArray[0] != 9 ||
+				codeArray[1] != 7 ||
+				codeArray[2] < 8
+			){
+				return false;
+			}
 			let sum = 0;
 			let digit = 0;
-			if(codeArray.length != 13)return false;
 			for(let i = 0;i < 12;i++){
 				if(i % 2 == 0){
 					sum += codeArray[i];
@@ -64,12 +99,48 @@ window.onload = () =>{
 		}
 
 	}
+	class Dataparser{
+		static parseNDLXML(xml){
+			let parser = new DOMParser();
+			let dom = parser.parseFromString(xml, 'text/xml');
+			let result = {};
+			result.find = dom.getElementsByTagName('numberOfRecords')[0].textContent > 0;
+			if(result.find){
+				let recodeData = parser.parseFromString(unescape(dom.getElementsByTagName('recordData')[0].textContent), 'text/xml');
+				result.title = recodeData.getElementsByTagName('dc:title')[0].textContent;
+				result.creator = recodeData.getElementsByTagName('dc:creator')[0].textContent;
+			}
+			return result;
 
-	let barcodeReader = new BarcodeReader(window.innerWidth, window.innerHeight, '#container', async code =>{
+		}
+	}
+
+	let barcodeReader = new BarcodeReader(window.innerWidth, window.innerHeight, '#viewer', async code =>{
+		let res;
+		try{
+			res = await superagent
+				.get('http://iss.ndl.go.jp/api/sru?operation=searchRetrieve')
+				.query({
+					query:`isbn=${code}`
+				})
+				.timeout({
+					response: 5000
+				})
+			;
+		}catch(e){
+			console.error(e);
+			await swal({
+				title: '国会図書館apiからデータを取得できませんでした',
+				type: 'error',
+				text: e
+			});
+			return true;
+		}
+		let bookData = Dataparser.parseNDLXML(res.text);
 		let result = await swal({
 			type: 'question',
 			title: 'これであってる?(自信なさげ)',
-			text: code,
+			text: bookData.find ? `${bookData.title} / ${bookData.creator}` : `ISBNCode: ${code}`,
 			showCancelButton: true,
 			cancelButtonColor: '#d33',
 			cancelButtonText: '合ってない',
@@ -90,7 +161,8 @@ window.onload = () =>{
 				console.error(e);
 				await swal({
 					title: 'データを送信できませんでした',
-					type: 'error'
+					type: 'error',
+					text: e
 				});
 				return true;
 			}
